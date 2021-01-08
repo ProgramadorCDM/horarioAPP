edfb30f59f5945fd1318742b50155b3e54137e8c
Jorge Mina <minajorge@hotmail.com>
2021-01-08 12:17:13 -0500

    Resumen Works!

--- src/app/pages/resumen/resumen.module.ts
+++ src/app/pages/resumen/resumen.module.ts
@@ -6,11 +6,10 @@ import { ResumenRoutingModule } from './resumen-routing.module';
 import { PrimengModule } from 'src/app/shared/primeng.module';
 /* Components */
 import { HomeComponent } from './components/home/home.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, ToolbarComponent],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     ResumenRoutingModule,
